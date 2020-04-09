b3bb0620845461269d87dbb986872a3963ac3c63
Patryk Sobow <patryk.sobow95@gmail.com>
2020-04-09 03:12:48 +0200

    Create CurrencyModel class

---
+++ CurrencyRates/CurrencyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyRates
+{
+    public class CurrencyModel
+    {
+        public string table { get; set; }
+        public string currency { get; set; }
+        public string code { get; set; }
+        public List<Rates> rates { get; set; }
+    }
+
+    public class Rates
+    {
+        public string no { get; set; }
+        public DateTime effectiveDate { get; set; }
+        public double mid { get; set; }
+    }
+}
