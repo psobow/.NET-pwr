b28b007d30f18dda7bfad9f5fa341a8bce4c8e88
Patryk Sobow <patryk.sobow95@gmail.com>
2020-05-10 21:47:17 +0200

    Create ClientNBP test suite and implement two tests

---
+++ CurrencyRates_testProject/Test_ClientNBP.cs
@@ -0,0 +1,73 @@
+using CurrencyRates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+
+namespace CurrencyRates_testProject
+{
+    [TestClass]
+    public class Test_ClientNBP
+    {
+        private ClientNBP clientNBP = ClientNBP.Instance;
+
+        // SUCCESS SCENARIO
+
+        // Current Values
+        // Eur
+        [TestMethod]
+        public async System.Threading.Tasks.Task shouldGetEurCurrencyAsync()
+        {
+            // Given
+
+            // When
+            string eurResponseJSON = await clientNBP.getCurrentEURAsync();
+
+            // Then
+            CurrencyModel EURFromWebAPI = JsonConvert.DeserializeObject<CurrencyModel>(eurResponseJSON);
+
+            Assert.IsNotNull(EURFromWebAPI);
+            Assert.IsNotNull(EURFromWebAPI.code);
+            Assert.IsNotNull(EURFromWebAPI.currency);
+            Assert.IsNotNull(EURFromWebAPI.Rates);
+            Assert.IsNotNull(EURFromWebAPI.table);
+
+            Assert.AreEqual(EURFromWebAPI.code, "EUR");
+        }
+
+        // USD
+
+        // GBP
+
+        // GOLD
+
+
+
+        // Period of time
+        // EUR
+        [TestMethod]
+        public async System.Threading.Tasks.Task shouldGetEurCurrencyPeriodOfTimeAsync()
+        {
+            // Given
+
+            // When
+            string eurResponseJSON = await clientNBP.getEURFromPeriodOfTime("2020-03-01", "2020-04-01");
+
+            // Then
+            CurrencyModel EURFromWebAPI = JsonConvert.DeserializeObject<CurrencyModel>(eurResponseJSON);
+
+            Assert.IsNotNull(EURFromWebAPI);
+            Assert.IsNotNull(EURFromWebAPI.code);
+            Assert.IsNotNull(EURFromWebAPI.currency);
+            Assert.IsNotNull(EURFromWebAPI.Rates);
+            Assert.IsNotNull(EURFromWebAPI.table);
+
+            Assert.AreEqual(EURFromWebAPI.code, "EUR");
+        }
+
+        // USD
+
+        // GBP
+
+        // GOLD
+    }
+}
